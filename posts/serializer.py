45120ca5a3c94866a00198350a151645c2d6413f
Hossein Shams <hosseineeshams@gmail.com>
2023-04-28 11:56:54 +0330

    feat: add comments and likes

--- posts/serializer.py
+++ posts/serializer.py
@@ -4,19 +4,51 @@ from django.conf import settings
 from django.core.files import File
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
-from posts.models import Post
+from posts.models import Post, Comment, Like
 from accounts.serializers import UserSerializer
 from .image_resource import gen_image
 
 import requests, uuid, os
 from django.core.files.temp import NamedTemporaryFile
+
+
+class UserShortField(serializers.RelatedField):
+    def to_representation(self, value):
+        return {'id': value.id, 'username': value.username}
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = UserShortField(many=False, read_only=True)
+
+    class Meta:
+        model = Like
+        fields = ('id', 'post', 'owner', 'created_at')
+        read_only_fields = ('id', 'created_at')
+
+    def create(self, validated_data):
+        validated_data['owner'] = self.context['request'].user
+        like_obj = Like.objects.create(**validated_data)
+        return like_obj
+
+
 class PostSerializer(ModelSerializer):
-    owner = UserSerializer(many=False, read_only=True)
+    owner = UserShortField(many=False, read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
+
+    def __init__(self, args, **kwargs):
+
+        # Don't return emails when listing users
+
+        if kwargs['context']['view'].action == 'list':
+            del self.fields['likes']
+
+        super().__init__(args, **kwargs)
 
     class Meta:
         model = Post
-        fields = ('id', 'description', 'owner', 'image', 'created_at', 'updated_at',)
-        read_only_fields = ['id', 'created_at', 'image', 'updated_at']
+        fields = (
+        'id', 'description', 'owner', 'likes', 'likes_count', 'comments_count', 'image', 'created_at', 'updated_at',)
+        read_only_fields = ['id', 'created_at', 'likes', 'image', 'likes_count', 'comments_count', 'updated_at']
 
     def create(self, validated_data):
         # set post owner
@@ -43,3 +75,13 @@ class PostSerializer(ModelSerializer):
         post_obj.image.save(os.path.basename(image_url) + '.jpg', File(img_temp), save=True)
 
         return post_obj
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = UserShortField(many=False, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'owner', 'description', 'created_at')
+        read_only_fields = ('id', 'created_at', 'post', 'owner')
+
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,23 @@
-from rest_framework import generics, permissions
-from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, viewsets, status
+from rest_framework.response import Response
 
-from .models import Post
-from posts.serializer import PostSerializer
+from .models import Post, Comment, Like
+from posts.serializer import PostSerializer, CommentSerializer
+from rest_framework.decorators import action
 
 from rest_framework import filters
 
+from .permissions import IsCommentOwnerOrReadOnly
 
-class PostViewSet(ModelViewSet):
+
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['description']
+    filterset_fields = ['owner']
 
     def get_queryset(self):
         user = self.request.user
@@ -23,7 +28,35 @@ class PostViewSet(ModelViewSet):
             if request_type == 'DELETE':
                 return Post.objects.filter(owner=user)
             elif request_type == "GET":
-                return Post.objects.all()
+                return Post.objects.prefetch_related("likes").all()
             else:
                 return Post.objects.none
 
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+
+        post = Post.objects.filter(id=pk).first()
+
+        like = post.likes.filter(owner=request.user).first()
+        if like is None:
+            like = Like(owner=request.user, post=post)
+            like.save()
+            post.likes_count += 1
+            post.save()
+            return Response({'message': 'Post liked successfully.'}, status=status.HTTP_201_CREATED)
+
+        like.delete()
+        post.likes_count -= 1
+        post.save()
+        return Response({'message': 'Post unliked successfully.'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsCommentOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user, post_id=self.kwargs['post_pk'])
+
+    def get_queryset(self):
+        return Comment.objects.filter(post_id=self.kwargs['post_pk'])
